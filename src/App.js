3dd6aa1aabb880fce189c16d82efb456255272dc
Roman Shauk <shauk60@gmail.com>
2019-01-25 16:05:45 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+var CONTACTS = [
+  {
+      id: 1,
+      name: 'Darth Vader',
+      phoneNumber: '+250966666666',
+      image: 'img/darth.png'
+  }, {
+      id: 2,
+      name: 'Princess Leia',
+      phoneNumber: '+250966344466',
+      image: 'img/leia.png'
+  }, {
+      id: 3,
+      name: 'Luke Skywalker',
+      phoneNumber: '+250976654433',
+      image: 'img/luke.png'
+  }, {
+      id: 4,
+      name: 'Chewbacca',
+      phoneNumber: '+250456784935',
+      image: 'img/chewbacca.png'
+  }
+];
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+class Contact extends Component{
+  render(){
+    return(
+      <li className="contact">
+        <img className="contact-image" alt='' src={this.props.image}/>
+        <div className="contact-info">
+          <div className="contact-name"> {this.props.name} </div>
+          <div className="contact-number"> {this.props.phoneNumber} </div>
+        </div>
+      </li>
+    )
   }
 }
+class ContactLIst extends Component{
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      displayedContacts: CONTACTS
+    };
+}
+  handleSearch=(event)=>{
+    var searchQuery = event.target.value.toLowerCase();
+    const displayedContacts = CONTACTS.filter(function(el) {
+      var searchValue = el.name.toLowerCase();
+      return searchValue.indexOf(searchQuery) !== -1;
+  });
+  this.setState({
+    displayedContacts: displayedContacts
+    });
+  }
+  render(){
+    return(
+      <div className="contacts">
+      <input type="text" placeholder="Search..." className="search-field" onChange={this.handleSearch} />
+      <ul className="contacts-list">
+          {
+             this.state.displayedContacts.map(function(el) {
+                  return <Contact
+                      key={el.id}
+                      name={el.name}
+                      phoneNumber={el.phoneNumber}
+                      image={el.image}
+                  />;
+             })
+          }
+      </ul>
+  </div>
+    )
+  }
+
+}
+export{
+    ContactLIst
+}
+
